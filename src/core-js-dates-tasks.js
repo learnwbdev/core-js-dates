00506f23ab0e333ccc8d3a14fd2ea9d5a4a4cf24
Alexandra Paramonova <alpar.wb@gmail.com>
2024-03-11 21:53:03 +0500

    feat: implement getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,14 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const formatTimeUnit = (timeUnitNum) => {
+    return timeUnitNum.toString().padStart(2, '0');
+  };
+  const hoursStr = formatTimeUnit(date.getHours());
+  const minutesStr = formatTimeUnit(date.getMinutes());
+  const secondsStr = formatTimeUnit(date.getSeconds());
+  return `${hoursStr}:${minutesStr}:${secondsStr}`;
 }
 
 /**
