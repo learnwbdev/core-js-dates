1bca9146687e96dcaf0b7b452b2225122d0f73c5
Alexandra Paramonova <alpar.wb@gmail.com>
2024-03-25 15:42:24 +0500

    feat: implement isLeapYear

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -281,8 +281,12 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getUTCFullYear();
+  const isYearDivisibleBy400 = year % 400 === 0;
+  const isYearDivisibleBy100 = year % 100 === 0;
+  const isYearDivisibleBy4 = year % 4 === 0;
+  return isYearDivisibleBy400 || (!isYearDivisibleBy100 && isYearDivisibleBy4);
 }
 
 module.exports = {
